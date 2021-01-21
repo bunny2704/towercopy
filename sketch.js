9f6f76b039444729bae21763df9edc6dc6f1cbe0
kiddo948 <75846247+kiddo948@users.noreply.github.com>
2021-01-21 16:10:41 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -126,7 +126,7 @@ function mouseDragged(){
         }
     }
     async function getBackgroundImg(){
-        var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata);
+        var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
         var  responseJSON = await response.json();
     
         var datetime = responseJSON.datetime;
